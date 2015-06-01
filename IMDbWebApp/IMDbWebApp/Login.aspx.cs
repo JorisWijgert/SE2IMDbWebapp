5b42d66f33abb6ddb845c7da189b5247198b19ae
Joris van de Wijgert <j.vandewijgert@student.fontys.nl>
2015-06-01 13:36:44 +0200

    basic - login

---
+++ IMDbWebApp/IMDbWebApp/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace IMDbWebApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            using (OracleConnection c = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                c.Open();
+                OracleCommand cmd = new OracleCommand("select * from gebruiker", c);
+                OracleDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        lblEmail.Text = reader[1].ToString();
+                    }
+                }
+                reader.Close();
+            }
+
+        }
+    }
+}
